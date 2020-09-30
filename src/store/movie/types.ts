abc120d7ad007aea6e61303aa562e6bd70a0e1cc
Luis Banda <Luis_Banda@epam.com>
2020-09-30 14:37:16 -0500

    Removing action types and using the enums type in the types of the interfaces.

--- src/store/movie/types.ts
+++ src/store/movie/types.ts
@@ -1,50 +1,50 @@
-import * as constants from './constants';
+import { ACTIONS } from './constants';
 import { IMovieData } from '../../interfaces/IMovieData';
 
 export interface IAddMovie {
-  type: constants.ADD_MOVIE;
+  type: ACTIONS.ADD_MOVIE;
   movieToAdd: IMovieData;
 }
 
 export interface IUpdateMovie {
-  type: constants.UPDATE_MOVIE;
+  type: ACTIONS.UPDATE_MOVIE;
   movieToUpdate: IMovieData;
 }
 
 export interface IDeleteMovie {
-  type: constants.DELETE_MOVIE;
+  type: ACTIONS.DELETE_MOVIE;
   movieId: number;
 }
 
 export interface ILoadingMovies {
-  type: constants.LOADING_MOVIES;
+  type: ACTIONS.LOADING_MOVIES;
   loading: boolean;
 }
 export interface ILoadMoviesSuccess {
-  type: constants.LOAD_MOVIES_SUCCESS;
+  type: ACTIONS.LOAD_MOVIES_SUCCESS;
   movies: IMovieData[];
   loading: boolean;
 }
 
 export interface ILoadMoviesError {
-  type: constants.LOADING_MOVIES_ERROR;
+  type: ACTIONS.LOADING_MOVIES_ERROR;
   loading: boolean;
   errorMessage: string;
 }
 
 export interface ISelectMovie {
-  type: constants.SELECT_MOVIE;
+  type: ACTIONS.SELECT_MOVIE;
   movie: IMovieData;
 }
 
 export interface IFilterReleaseDateAndRating {
-  type: constants.FILTER_BY_RELEASE_DATE_AND_RATING;
+  type: ACTIONS.FILTER_BY_RELEASE_DATE_AND_RATING;
   releaseDate: string;
   rating: number;
 }
 
 export interface ISortByGenre {
-  type: constants.SORT_BY_GENRE;
+  type: ACTIONS.SORT_BY_GENRE;
   genre: string;
 }
 
